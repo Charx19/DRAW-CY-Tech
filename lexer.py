496aadcdd43bc494ddb6019f339f885c8e6a2d91
Noor-Lisa <130580271+Noor-Lisa@users.noreply.github.com>
2024-11-14 15:57:05 +0100

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 
-# Liste des tokens
+# List of tokens
 tokens = [
     'MOVE_TO',
     'LINE_TO',
@@ -9,27 +9,48 @@ tokens = [
     'NUMBER',
     'STRING',
     'NEWLINE',
+    'RECTANGLE', 
+    'TRIANGLE', 
+    'COLOR', 
+    'SINGLELINE_COMMENT',
+    'MULTILINE_COMMENT',
 ]
 
-# Définition des expressions régulières pour les tokens
+# Definition of regular expressions for the tokens
 t_MOVE_TO = r'move_to'
 t_LINE_TO = r'line_to'
 t_SET_COLOR = r'set_color'
 t_CIRCLE = r'circle'
+t_RECTANGLE = r'rectangle'
+t_TRIANGLE = r'triangle'
 t_NUMBER = r'\d+'
-t_STRING = r'\".*?\"'  # Chaînes entre guillemets
+t_STRING = r'\".*?\"'  # Strings enclosed in quotes
 
-# Ignorer les espaces et tabulations
+# Colors
+def t_COLOR(t):
+    r'red|blue|green|black|yellow'
+    return t
+
+# Ignore spaces and tabs
 t_ignore = ' \t'
 
-# Traitement des nouvelles lignes
+# Handling newlines
 def t_NEWLINE(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# Gestion des erreurs
+def t_SINGLELINE_COMMENT(t):
+    r'//.*'
+    pass  #  Ignore single-line comments
+
+def t_MULTILINE_COMMENT(t):
+    r'/\*.*?\*/'
+    pass  # Ignore multi-line comments
+
+# Error handling
 def t_error(t):
     print(f"Erreur de lexing à la ligne {t.lineno}: {t.value}")
+    t.lexer.skip(1)
 
-# Création du lexer
+# Creating the lexer
 lexer = lex.lex()
