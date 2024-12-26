82e5c06db5460f66b7f31c4d4682bbb3db2c6052
Loueva <133343765+louevaberanger@users.noreply.github.com>
2024-12-26 22:33:46 +0100

    Add files via upload

---
+++ include/graphic.h
@@ -0,0 +1,23 @@
+#ifndef GRAPHIC
+#define GRAPHIC
+
+#include <SDL.h>
+typedef struct {
+    char color[20];
+    int x;
+    int y;
+} Cursor;
+
+// Initialisation et fermeture
+void init_graphics();
+void close_graphics();
+
+// Fonctions graphiques
+void move_to(Cursor a, int x, int y);
+void line_to(Cursor a, int x, int y);
+void circle(Cursor a, int radius);
+void cursor(const char* color, int x, int y);
+void move_by(Cursor a, int x, int y);
+void line_by(Cursor a, int x, int y);
+
+#endif
